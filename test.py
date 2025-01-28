9cce0b490c662a2d8402ded016b8c28d010c8b2b
ovistetom <ovistetom@proton.me>
2025-01-28 13:11:19 +0100

    ovstm: progress.

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pyroomacoustics as pra
+import random
+import itertools 
+import torch
 import torchaudio
 import torchaudio.transforms
-import random
-import os
-import tqdm
 
 
 def random_room_dimensions():
@@ -14,25 +13,25 @@ def random_room_dimensions():
     z = random.uniform(2.5, 5)
     return np.array([x, y, z])
 
-
-def random_head_position(room_dim):
+def random_head_position(room_dim, rdtw=0.125):
     rdx, rdy, _ = room_dim
-    x = random.uniform(0.125*rdx, 0.875*rdx)
-    y = random.uniform(0.125*rdy, 0.875*rdy)
+    x = random.uniform(rdtw*rdx, (1-rdtw)*rdx)
+    y = random.uniform(rdtw*rdy, (1-rdtw)*rdy)
     z = random.uniform(1, 1.75)
     return np.array([x, y, z])
 
+def random_head_angle():
+    return random.uniform(0, 2*np.pi)
 
 def random_mouth_position(head_pos, head_ang):
     hpx, hpy, hpz = head_pos
     rdx = random.uniform(-0.01, 0.01)
     rdy = random.uniform(0.11, 0.15)
-    rdz = random.uniform(0.02, 0.04)
+    rdz = random.uniform(-0.04, -0.02)
     x = hpx + rdx*np.cos(head_ang) - rdy*np.sin(head_ang)
     y = hpy + rdx*np.sin(head_ang) + rdy*np.cos(head_ang)
-    z = hpz - rdz
+    z = hpz + rdz
     return np.array([x, y, z])
-
 
 def random_ears_position(head_pos, head_ang):
     hpx, hpy, hpz = head_pos
@@ -40,7 +39,6 @@ def random_ears_position(head_pos, head_ang):
     ear_center_l = np.array([hpx - rdx*np.cos(head_ang), hpy - rdx*np.sin(head_ang), hpz])
     ear_center_r = np.array([hpx + rdx*np.cos(head_ang), hpy + rdx*np.sin(head_ang), hpz])
     return np.array([ear_center_l, ear_center_r])
-
 
 def define_mics_position(ears_pos):
     ear_center_l, ear_center_r = ears_pos
@@ -52,58 +50,81 @@ def define_mics_position(ears_pos):
     mic_r_2 = np.array([rex, rey, rez + 0.01])
     return np.array([mic_l_1, mic_l_2, mic_r_1, mic_r_2])
 
-
-def random_distractor_position(room_dim, head_pos):
+def random_distractor_position(room_dim, head_pos, rdtw=0.125):
     rdx, rdy, _ = room_dim
     hpx, hpy, _ = head_pos
-    angl = random.uniform(0, 2*np.pi)
+    angle = random.uniform(0, 2*np.pi)
 
-    dx = np.cos(angl)*1
+    dx = np.cos(angle)*1
     if dx > 0:
         x_min = hpx + dx
-        x_max = 0.875*rdx
+        x_max = (1-rdtw)*rdx
         if x_min > x_max:
-            x_min = 0.125*rdx
+            x_min = rdtw*rdx
             x_max = hpx - dx     
     else:
-        x_min = 0.125*rdx
+        x_min = rdtw*rdx
         x_max = hpx + dx
         if x_min > x_max:
             x_min = hpx - dx
-            x_max = 0.875*rdx
+            x_max = (1-rdtw)*rdx
     x = random.uniform(x_min, x_max)
 
-    dy = np.sin(angl)*1
+    dy = np.sin(angle)*1
     if dy > 0:
         y_min = hpy + dy
-        y_max = 0.875*rdy
+        y_max = (1-rdtw)*rdy
         if y_min > y_max:
-            y_min = 0.125*rdy
+            y_min = rdtw*rdy
             y_max = hpy - dy
     else:
-        y_min = 0.125*rdy
+        y_min = rdtw*rdy
         y_max = hpy + dy
         if y_min > y_max:
             y_min = hpy - dy
-            y_max = 0.875*rdy
+            y_max = (1-rdtw)*rdy
     y = random.uniform(y_min, y_max)
     
     z = random.uniform(1, 1.75)
     return np.array([x, y, z])
 
+def random_snr(a=-5, b=5):
+    return random.uniform(a, b)
+
+def random_rt60(room_dim):
+    v = np.prod(room_dim)
+    s = 2 * np.sum([l1 * l2 for l1, l2 in itertools.combinations(room_dim, 2)])
+    c = pra.parameters._constants_default['c']
+    sc = 24
+    a = v*sc*np.log(10)/(c*s)
+    return random.uniform(a, 1.0)
+
+
 if __name__ == '__main__':
 
+    SR = 16000
+    is_anechoic = False
+
+    # Create acoustic scene.
     room_dim = random_room_dimensions()
     head_pos = random_head_position(room_dim)
-    head_ang = random.uniform(0, 2*np.pi)
+    head_ang = random_head_angle()
     ears_pos = random_ears_position(head_pos, head_ang)
     mics_pos = define_mics_position(ears_pos)
     mouth_pos = random_mouth_position(head_pos,  head_ang)
-    distractor_pos = random_distractor_position(room_dim, head_pos)    
+    distractor_pos = random_distractor_position(room_dim, head_pos)
 
-    x = np.zeros(1000)
+    # Create shoebox room.
+    if is_anechoic:
+        e_absorption, max_order = 1.0, 0
+    else:
+        rt60 = random_rt60(room_dim)
+        e_absorption, max_order = pra.inverse_sabine(rt60, room_dim)
+    room = pra.ShoeBox(room_dim, fs=SR, materials=pra.Material(e_absorption), max_order=max_order)
 
-    for i in range(1000):
-        room_dim = random_room_dimensions()
-        head_pos = random_head_position(room_dim)
-        distractor_pos = random_distractor_position(room_dim, head_pos)
+    # room.add_source(mouth_pos, signal=signal_clean, delay=0.0)
+    # room.add_microphone_array(mics_pos.T)
+    # room.add_source(distractor_pos, signal=signal_noise, delay=0.0)
+
+    # room.simulate()
+    # signal_mixed = room.mic_array.signals
