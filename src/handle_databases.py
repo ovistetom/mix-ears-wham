80afd63b9d0172df40613a145f6e3475d4c85dab
ovistetom <ovistetom@proton.me>
2025-02-05 12:53:31 +0100

    ovstm: Improve database preprocessing.

--- src/handle_databases.py
+++ src/handle_databases.py
@@ -66,21 +66,30 @@ def segment_audio_array(audio_array, segment_length_in_s=20.0, fade_length_in_s=
     return faded_segments
 
 
+def find_start_index(audio, threshold=0.05):
+    """Find the index in an audio tensor when 0.05*maximum is reached."""
+    max_value = audio.max()
+    start_index = torch.where(audio > threshold*max_value)[0][0]
+    return start_index.item()
+
+def slice_end_of_audio_segment(audio_tensor, sr, len_in_s=4.0, fade=True, padding_mode='constant'):
+    """Slice the end of an audio segment to a given length. Apply fade-out and padding to the segment if necessary."""
+    audio_length = audio_tensor.size(0)
+    target_length = int(sr*len_in_s)
+    if audio_length > target_length:
+        return audio_tensor[:target_length]
+    else:
+        fader = tt.Fade(fade_in_len=0, fade_out_len=int(sr*random.uniform(0.01, 0.04)), fade_shape='linear')
+        audio_tensor = fader(audio_tensor) if fade else audio_tensor
+        audio_padded = ff.pad(audio_tensor, (0, target_length-audio_length), mode=padding_mode)
+        return audio_padded
+
 def process_vctk(vctk_root):
     """Split the VCTK database into training, validation and test subsets.
     Load all FLAC audio files in VCTK, slice them in segments of length 4s, and save the resulting audio files."""
 
     vctk_path = os.path.join(vctk_root, 'wav48_silence_trimmed')
     speaker_list = [ d for d in os.listdir(vctk_path) if os.path.isdir(os.path.join(vctk_path, d)) ] 
-
-    def find_start_index(audio, threshold=0.1):
-        max_value = audio.max()
-        start_index = torch.where(audio > threshold*max_value)[0][0]
-        return start_index.item() 
-    
-    fade_len_in_s = 0.25
-    fade_len = int(SR * fade_len_in_s)
-    fade_transform = tt.Fade(fade_in_len=fade_len, fade_out_len=fade_len, fade_shape='linear')
     
     num_speakers = len(speaker_list)
     random.shuffle(speaker_list)
@@ -109,13 +118,15 @@ def process_vctk(vctk_root):
                 audio = resample_transform(audio)
                 # Find beginning of utterance.
                 start_index = find_start_index(audio)
-                start_index = max(0, start_index - int(0.25*SR))
+                start_index = max(0, start_index - int(SR*random.uniform(0.125, 0.25)))
                 # Slice 4s segment from beginning of utterance.
                 audio = audio[start_index:]
-                audio = audio[:4*SR] if audio.size(0) > 4*SR else ff.pad(audio, (0, 4*SR-audio.size(0)))
+                audio = slice_end_of_audio_segment(audio, SR, len_in_s = 4.0 - random.uniform(0.125, 0.25))
+                audio = slice_end_of_audio_segment(audio, SR, len_in_s = 4.0)
                 # Normalize and fade.
                 audio /= audio.abs().max()
-                audio = fade_transform(audio)
+                fader = tt.Fade(fade_in_len=int(SR*0.125), fade_out_len=int(SR*0.125), fade_shape='linear')                
+                audio = fader(audio)
                 # Save segment.
                 file_path_dst = os.path.join(speaker_path_dst, file_name)
                 torchaudio.save(file_path_dst, audio.unsqueeze(0), SR)            
@@ -123,15 +134,6 @@ def process_vctk(vctk_root):
 def process_lisp(lisp_root):
     """Split the LibriSpeech database into training, validation and test subsets.
     Load all FLAC audio files in LibriSpeech, slice them in segments of length 4s, and save the resulting audio files."""  
-
-    def find_start_index(audio, threshold=0.1):
-        max_value = audio.max()
-        start_index = torch.where(audio > threshold*max_value)[0][0]
-        return start_index.item()
-
-    fade_len_in_s = 0.25
-    fade_len = int(SR * fade_len_in_s)
-    fade_transform = tt.Fade(fade_in_len=fade_len, fade_out_len=fade_len, fade_shape='linear')
 
     for (subset_name, new_subset_name) in [('test-clean', 'tst'), ('train-clean-100', 'trn'), ('dev-clean', 'val')]:
         lisp_path = os.path.join(lisp_root, subset_name)
@@ -154,13 +156,15 @@ def process_lisp(lisp_root):
                     audio = resample_transform(audio)
                     # Find beginning of utterance.
                     start_index = find_start_index(audio)
-                    start_index = max(0, start_index - int(0.25*SR))
+                    start_index = max(0, start_index - int(SR*random.uniform(0.125, 0.25)))
                     # Slice 4s segment from beginning of utterance.
                     audio = audio[start_index:]
-                    audio = audio[:4*SR] if audio.size(0) > 4*SR else ff.pad(audio, (0, 4*SR-audio.size(0)))
+                    audio = slice_end_of_audio_segment(audio, SR, len_in_s = 4.0 - random.uniform(0.125, 0.25))
+                    audio = slice_end_of_audio_segment(audio, SR, len_in_s = 4.0)
                     # Normalize and fade.
                     audio /= audio.abs().max()
-                    audio = fade_transform(audio)
+                    fader = tt.Fade(fade_in_len=int(SR*0.125), fade_out_len=int(SR*0.125), fade_shape='linear')                
+                    audio = fader(audio)
                     # Save segment.
                     file_path_dst = os.path.join(speaker_path_dst, file_name)
                     torchaudio.save(file_path_dst, audio.unsqueeze(0), SR)                 
@@ -196,13 +200,15 @@ def process_dmnd(dmnd_root):
                 file_path_src = os.path.join(envt_path_src, file_name)
                 audio, sr = torchaudio.load(file_path_src, channels_first=True)
                 # Slice audio in 16s segments.
-                segments_16s = segment_audio_array(audio, segment_length_in_s=16.0, fade_length_in_s=0.0, sample_rate=sr)
+                segments_16s = segment_audio_array(audio, segment_length_in_s=16, fade_length_in_s=0, sample_rate=sr)
                 for i, segment_i in enumerate(segments_16s):
                     # Slice segment in 4-channel signals of length 4s.
-                    segment_4s = segment_audio_array(segment_i, segment_length_in_s=4.0, fade_length_in_s=0.25, sample_rate=sr)
+                    segment_4s = segment_audio_array(segment_i, segment_length_in_s=4, fade_length_in_s=0, sample_rate=sr)
                     segment_4s = segment_4s.squeeze(1)
-                    # Divide each channel by max.
+                    # Normalize and fade.
                     segment_4s /= torch.max(segment_4s)
+                    fader = tt.Fade(fade_in_len=int(sr*0.03125), fade_out_len=int(sr*0.03125), fade_shape='linear')
+                    segment_4s = fader(segment_4s)
                     # Save 4-channel segment.
                     file_path_dst = os.path.join(envt_path_dst, f'{file_name[:-4]}_{i:02}.flac')
                     torchaudio.save(file_path_dst, segment_4s, sr)
@@ -211,10 +217,6 @@ def process_dmnd(dmnd_root):
 def process_wham(wham_root):
     """Split the WHAM database into training, validation and test subsets.
     Load all audio files in WHAM, slice them in segments of length 4s, and save the resulting audio files."""  
-
-    fade_len_in_s = 0.25
-    fade_len = int(SR * fade_len_in_s)
-    fade_transform = tt.Fade(fade_in_len=fade_len, fade_out_len=fade_len, fade_shape='linear')
     
     for (subset_name, new_subset_name) in [('tt', 'tst'), ('tr', 'trn'), ('cv', 'val')]:
        
@@ -225,28 +227,28 @@ def process_wham(wham_root):
         os.makedirs(wham_path_dst, exist_ok=True) 
 
         for i, (file_name_1, file_name_2, file_name_3, file_name_4) in enumerate(zip(wham_list[::4], wham_list[1::4], wham_list[2::4], wham_list[3::4])):
-            # Load audio files.
+            # Load audio files (keep only first channel).
             file_path_1 = os.path.join(wham_path_src, file_name_1)
             file_path_2 = os.path.join(wham_path_src, file_name_2)
             file_path_3 = os.path.join(wham_path_src, file_name_3)
             file_path_4 = os.path.join(wham_path_src, file_name_4)
-            audio_1, sr_1 = torchaudio.load(file_path_1, channels_first=True)
-            audio_2, sr_2 = torchaudio.load(file_path_2, channels_first=True)
-            audio_3, sr_3 = torchaudio.load(file_path_3, channels_first=True)
-            audio_4, sr_4 = torchaudio.load(file_path_4, channels_first=True)
-            assert sr_1 == sr_2 == sr_3 == sr_4 == SR, "Audio files must have the same sample rate."
-            # Keep only first channel and first 4s of each audio file (pad if necessary).
-            audio_1 = audio_1[0, :4*SR] if audio_1.size(1) > 4*SR else ff.pad(audio_1, (0, 4*SR-audio_1.size(1)), mode='reflect')[0]
-            audio_2 = audio_2[0, :4*SR] if audio_2.size(1) > 4*SR else ff.pad(audio_2, (0, 4*SR-audio_2.size(1)), mode='reflect')[0]
-            audio_3 = audio_3[0, :4*SR] if audio_3.size(1) > 4*SR else ff.pad(audio_3, (0, 4*SR-audio_3.size(1)), mode='reflect')[0]
-            audio_4 = audio_4[0, :4*SR] if audio_4.size(1) > 4*SR else ff.pad(audio_4, (0, 4*SR-audio_4.size(1)), mode='reflect')[0]
+            audio_1 = torchaudio.load(file_path_1, channels_first=True)[0][0]
+            audio_2 = torchaudio.load(file_path_2, channels_first=True)[0][0]
+            audio_3 = torchaudio.load(file_path_3, channels_first=True)[0][0]
+            audio_4 = torchaudio.load(file_path_4, channels_first=True)[0][0]
+            # Keep only first 4s of each audio file (pad if necessary).
+            audio_1 = slice_end_of_audio_segment(audio_1, SR, len_in_s=4, fade=False, padding_mode='reflect')
+            audio_2 = slice_end_of_audio_segment(audio_2, SR, len_in_s=4, fade=False, padding_mode='reflect')
+            audio_3 = slice_end_of_audio_segment(audio_3, SR, len_in_s=4, fade=False, padding_mode='reflect')
+            audio_4 = slice_end_of_audio_segment(audio_4, SR, len_in_s=4, fade=False, padding_mode='reflect')
             # Create 4-channel segment of length 4s, normalize and fade.
             segment_4s = torch.stack((audio_1, audio_2, audio_3, audio_4), dim=0)
             segment_4s = segment_4s / torch.max(segment_4s, dim=1, keepdim=True)[0]
-            segment_4s = fade_transform(segment_4s)
+            fader = tt.Fade(fade_in_len=int(SR*0.03125), fade_out_len=int(SR*0.03125), fade_shape='linear')
+            segment_4s = fader(segment_4s)
             # Save segment.
             file_path_dst = os.path.join(wham_path_dst, f"{i:05}.flac")
-            torchaudio.save(file_path_dst, segment_4s, sr_1)
+            torchaudio.save(file_path_dst, segment_4s, SR)
 
 
 def parse_vctk(vctk_root, subset='trn'):
@@ -334,5 +336,7 @@ if __name__ == '__main__':
     dmnd_root = r"/home/ovistetom/Documents/Databases_Local/DMND/DEMAND"
     wham_root = r"/home/ovistetom/Documents/Databases_Local/WHAM/wham_noise"
 
-    # process_vctk(vctk_root)
-    process_wham(wham_root)
+    process_vctk(vctk_root)
+    # process_lisp(lisp_root)
+    # process_dmnd(dmnd_root)
+    # process_wham(wham_root)
